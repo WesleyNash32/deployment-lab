b9ababbfc42a3ce07415dc2fda51d12b1367ed67
Wesley Nash <bruticus32@gmail.com>
2022-07-20 15:02:25 -0700

    deployed_updated

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ const path = require('path')
 
 const app = express()
 
-const port = 4005
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const port = process.env.PORT || 4005
 
 app.listen(port, () => {
     console.log(`Server is docked at 4005`)
